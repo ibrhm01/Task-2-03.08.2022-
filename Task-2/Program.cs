93c788d26a885f69245324556023e5ecc34685e2
Ibrahim Aliyev <ibrahimma@code.edu.az>
2022-08-03 17:53:22 +0400

    Task-2

---
+++ Task-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;           
+            string str = Console.ReadLine();
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'a')
+                {
+                    cnt++;
+                    Console.WriteLine("There is 'a' in the text");
+                    break;
+                }
+            }
+            if(cnt==0) Console.WriteLine("There isn't 'a' in the text");
+
+        }
+    }
+}
